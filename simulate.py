31d978d39ece1a0abdc73ed6e0b61b171670cbfb
Jackson Dean <Jackson.Dean@uvm.edu>
2022-01-18 11:16:06 -0500

    open physics client in gui

--- simulate.py
+++ simulate.py
@@ -1,5 +1,11 @@
 import pybullet as p
+import time
+
+simulation_fps = 60
 
 # create physics engine client
 physicsClient = p.connect(p.GUI)
+for step in range(1000):
+    p.stepSimulation()
+    time.sleep(1./simulation_fps)
 p.disconnect()
