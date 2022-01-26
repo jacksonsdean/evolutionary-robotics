3f939f54032f3d20d7352b6f906bd8f713e7174c
Jackson Dean <Jackson.Dean@uvm.edu>
2022-01-26 11:31:19 -0500

    change style of line for visualization

--- analyze.py
+++ analyze.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 backLegSensorValues =  np.load("data/backLegSensorValues.npy")
 frontLegSensorValues =  np.load("data/frontLegSensorValues.npy")
 plt.plot(backLegSensorValues , label="Back Leg", linewidth=3.5)
-plt.plot(frontLegSensorValues, label="Front Leg")
+plt.plot(frontLegSensorValues, label="Front Leg", linestyle="--")
 plt.legend()
 plt.show()
