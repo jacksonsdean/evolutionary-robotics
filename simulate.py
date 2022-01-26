01be4de6ff3046ea949dddfabbe15e25b1af2340
Jackson Dean <Jackson.Dean@uvm.edu>
2022-01-26 11:04:39 -0500

    add max simulation length

--- simulate.py
+++ simulate.py
@@ -2,8 +2,10 @@ import pybullet as p
 import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
+import numpy as np
 
 simulation_fps = 600
+simulation_length = 1000
 
 # create physics engine client
 physicsClient = p.connect(p.GUI)
@@ -23,14 +25,22 @@ p.loadSDF("world.sdf")
 
 # prepare sensors for robot with id robotId
 pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = np.zeros(simulation_length)
 
+step = 0
 while True:
     try:
         p.stepSimulation()
         # sensors:
         backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-        print("BackLeg:", backLegTouch)
+        backLegSensorValues[step] = pyrosim.Get_Touch_Sensor
+        # print("BackLeg:", backLegTouch)
+        
+        step+=1
+        if step >= simulation_length:
+            break
         time.sleep(1./simulation_fps) # sleep
+        
     except KeyboardInterrupt:
         break
     except p.error as e:
@@ -39,3 +49,5 @@ while True:
         break
 if p.isConnected():
     p.disconnect()
+
+print(backLegSensorValues)
