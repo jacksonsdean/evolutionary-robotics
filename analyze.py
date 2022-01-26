4782b7d776bccb3234145dffdced3e978587e7a0
Jackson Dean <Jackson.Dean@uvm.edu>
2022-01-26 11:19:19 -0500

    graph data

--- analyze.py
+++ analyze.py
@@ -2,4 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 backLegSensorValues =  np.load("data/backLegSensorValues.npy")
-print(backLegSensorValues)
+plt.plot(backLegSensorValues)
+plt.show()
