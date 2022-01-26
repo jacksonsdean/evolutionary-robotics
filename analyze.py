dce2b5639e990b74b5beeeaa8dc4cbf3879e0a37
Jackson Dean <Jackson.Dean@uvm.edu>
2022-01-26 11:25:10 -0500

    plot data

--- analyze.py
+++ analyze.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 backLegSensorValues =  np.load("data/backLegSensorValues.npy")
 frontLegSensorValues =  np.load("data/frontLegSensorValues.npy")
-plt.plot(backLegSensorValues , label="Back Leg")
+plt.plot(backLegSensorValues , label="Back Leg", linewidth=3.5)
 plt.plot(frontLegSensorValues, label="Front Leg")
 plt.legend()
 plt.show()
