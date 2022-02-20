910a8ee426ef5527701443cf78e1c268a7a6d807
Jackson <Jackson.Dean@uvm.edu>
2022-02-20 18:58:33 -0500

    add generate_brain

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ def Create_World():
     pyrosim.Send_Cube(name="Box", pos=[x-2., y+2., z], size=[length, width, height])
     pyrosim.End()
     
-def Create_Robot():
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
     pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
@@ -18,6 +18,20 @@ def Create_Robot():
     pyrosim.Send_Cube(name="FrontLeg", pos=[.5, 0, -.5], size=[length, width, height])
     pyrosim.End()
 
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+    pyrosim.End()
 
-Create_World()
-Create_Robot()
+def Create_Robot():
+    Generate_Body()
+    Generate_Brain()
+
+
+def Generate():
+    print("Generating world and robot")
+    Create_World()
+    Create_Robot()
+
+if __name__ == '__main__':
+    Generate()
--- simulate.py
+++ simulate.py
@@ -1,4 +1,9 @@
+import sys
 from simulation import Simulation
+from generate import Generate
 if __name__ == '__main__':
+    args = sys.argv[1:]
+    if "--generate" in args:
+        Generate()
     sim = Simulation()
     sim.run()
